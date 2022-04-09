083b5636d6ffe18638d29ff8abb5db55a794809f
saikiranreddy <saikiranreddy@gmail.com>
2022-04-09 17:55:36 +0530

    [kiran]-Added UC1 in SnakeAndLadder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.StartPosition();
+            
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        public int start_Position = 0;
+        public void StartPosition()
+        {
+            Console.WriteLine("Game starts now");
+            Console.WriteLine("Starting position of player is: " + start_Position);
+        }
+    }
+}
