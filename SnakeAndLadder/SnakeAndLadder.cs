060f421cdaafe634a252ed6f171973dcdb2798b8
saikiranreddy <saikiranreddy@gmail.com>
2022-04-13 00:10:41 +0530

    [kiran]-Added UC4 in SnakeAndLadder

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,31 +8,40 @@ namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
     {
-        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POISITION = 100;
         public int player_Position = 0, DieRoll;
         public void StartPlaying()
         {
             Console.WriteLine("Starting position of a player is : " + player_Position);
             Random random = new Random();
-            DieRoll = random.Next(1, 7);
-            Console.WriteLine("Player gets the Die Number : " + DieRoll);
 
-            switch (random.Next(0, 3))
+            while (player_Position < WINNING_POISITION)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No Play");
-                    break;
+                DieRoll = random.Next(1, 7);
+                Console.WriteLine("Player gets the Die Number : " + DieRoll);
 
-                case LADDER:
-                    player_Position += DieRoll;
-                    Console.WriteLine("Ladder: " + player_Position);
-                    break;
-                case SNAKE:
-                    player_Position -= DieRoll;
-                    Console.WriteLine("Snake: " + player_Position);
-                    break;
+                switch (random.Next(0, 3))
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
+
+                    case LADDER:
+                        player_Position += DieRoll;
+                        Console.WriteLine("Ladder: " + player_Position);
+                        break;
+
+                    case SNAKE:
+                        player_Position -= DieRoll;
+                        Console.WriteLine("Snake: " + player_Position);
+                        if (player_Position < 0)
+                        {
+                            player_Position = 0;
+                        }
+                        break;
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
         }
     }
 }
